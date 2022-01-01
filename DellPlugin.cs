5daa42cf7a8262d89d1b3a97de1d0480e316da9e
Rem0o <rem.merc@outlook.com>
2022-01-01 14:58:26 -0500

    Dispose pattern to safely release unmanaged resources

--- DellPlugin.cs
+++ DellPlugin.cs
@@ -1,16 +1,19 @@
 ﻿using DellFanManagement.Interop;
 using FanControl.Plugins;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace FanControl.DellPlugin
 {
-    public class DellPlugin : IPlugin
+    public class DellPlugin : IPlugin, IDisposable
     {
         private const string SYS_FILE = "bzh_dell_smm_io_x64.sys";
         private bool _dellInitialized;
         private FileInfo _copiedSysFile;
+        private Boolean m_DisposedValue;
 
         public string Name => "Dell";
 
@@ -23,13 +26,19 @@ namespace FanControl.DellPlugin
                 DellFanLib.Shutdown();
 
                 _copiedSysFile.Delete();
+                _copiedSysFile = null;
+                _dellInitialized = false;
             }
         }
 
         public void Initialize()
         {
+            var copyLocation = Path.Combine(Directory.GetCurrentDirectory(), SYS_FILE);
+            if (File.Exists(copyLocation))
+                File.Delete(copyLocation);
+
             FileInfo sysFile = new FileInfo(typeof(DellFanLib).Assembly.Location).Directory.GetFiles(SYS_FILE).FirstOrDefault();
-            _copiedSysFile = sysFile.CopyTo(Path.Combine(Directory.GetCurrentDirectory(), SYS_FILE), true);
+            _copiedSysFile = sysFile.CopyTo(copyLocation, true);
 
             _dellInitialized = DellFanLib.Initialize();
 
@@ -52,6 +61,36 @@ namespace FanControl.DellPlugin
                 _container.ControlSensors.AddRange(fanControls);
                 _container.FanSensors.AddRange(fanSensors);
             }
+        }
+
+        protected virtual void Dispose(Boolean disposing)
+        {
+            if (!m_DisposedValue)
+            {
+                if (disposing)
+                {
+                  // TODO: dispose managed state (managed objects)
+                }
+
+              // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+              // TODO: set large fields to null
+              Close();
+              m_DisposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+         ~DellPlugin()
+         {
+             // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+             Dispose(disposing: false);
+         }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
